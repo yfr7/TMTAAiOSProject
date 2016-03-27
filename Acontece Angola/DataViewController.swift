f4eb105332d803c5f6cd916d91428143183c4327
yfr7 <yfernandes7@gmail.com>
2016-03-28 00:51:29 +0100

    Initial Commit

---
+++ Acontece Angola/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Acontece Angola
+//
+//  Created by Yves Fernandes on 3/28/16.
+//  Copyright © 2016 Tato Mitue Technologies Inc. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ Acontece AngolaTests/Acontece_AngolaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Acontece_AngolaTests.swift
+//  Acontece AngolaTests
+//
+//  Created by Yves Fernandes on 3/28/16.
+//  Copyright © 2016 Tato Mitue Technologies Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Acontece_Angola
+
+class Acontece_AngolaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
